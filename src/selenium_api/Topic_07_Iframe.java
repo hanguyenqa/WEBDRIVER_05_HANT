50f91e8933d61c9e158187ba3b85428979ed9a27
Ha Nguyen <thanhhanguyenc10d@gmail.com>
2018-08-20 18:12:14 +0700

    Topic 7- part 2 window - TC 02.03

--- src/selenium_api/Topic_07_Iframe.java
+++ src/selenium_api/Topic_07_Iframe.java
@@ -4,6 +4,7 @@ import org.testng.annotations.Test;
 import org.testng.annotations.BeforeClass;
 
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -27,7 +28,6 @@ public class Topic_07_Iframe {
 
 			driver.switchTo().frame(notificationIframe.get(0));
 			driver.findElement(By.xpath("//div[@id='div-close']")).click();
-			;
 			driver.switchTo().defaultContent();
 			System.out.println("Popup displayed");
 		}
@@ -77,21 +77,94 @@ public class Topic_07_Iframe {
 	public void TC_02() {
 
 		driver.get("http://daominhdam.890m.com/");
-		
+
 		String parentWindow = driver.getWindowHandle();
 		WebElement openGoogle = driver.findElement(By.xpath("//a[text()='Click Here']"));
-		
+
 		openGoogle.click();
+
+		switchToWindowByTitle("Google");
+
+		Assert.assertEquals(driver.getTitle(), "Google");
+
+		closeAllWithoutParentWindows(parentWindow);
+
+	}
+
+	@Test
+	public void TC_03() {
+
+		driver.get("http://www.hdfcbank.com/");
+
+		String parentWindow = driver.getWindowHandle();
+		// Close popup nếu có hiển thị
+		List<WebElement> notificationIframe = driver.findElements(By.xpath("//iframe[@id='vizury-notification-template']"));
+		if (notificationIframe.size() > 0) {
+
+			driver.switchTo().frame(notificationIframe.get(0));
+			driver.findElement(By.xpath("//div[@id='div-close']")).click();
+			driver.switchTo().defaultContent();
+			System.out.println("Popup displayed");
+		}
+
+		WebElement agriLink = driver.findElement(By.xpath("//a[text()='Agri']"));
+		agriLink.click();
+
+		switchToWindowByTitle("HDFC Bank Kisan Dhan Vikas e-Kendra");
+
+		WebElement accountDetailLink = driver.findElement(By.xpath("//p[text()='Account Details']"));
+		accountDetailLink.click();
+
+		switchToWindowByTitle("Welcome to HDFC Bank NetBanking");
+		WebElement footerIframe = driver.findElement(By.xpath("//frame[@name='footer']"));
+
+		driver.switchTo().frame(footerIframe);
+
+		WebElement policyLink = driver.findElement(By.xpath("//a[text()='Privacy Policy']"));
+
+		policyLink.click();
 		
+		switchToWindowByTitle("HDFC Bank - Leading Bank in India, Banking Services, Private Banking, Personal Loan, Car Loan");
 		
+		WebElement csrLink = driver.findElement(By.xpath("//a[text()='CSR']"));
+		csrLink.click();
 		
+		closeAllWithoutParentWindows(parentWindow);
 		
+		System.out.println(driver.getTitle());
+
+	}
+
+	public void switchToWindowByTitle(String title) {
+		Set<String> allWindows = driver.getWindowHandles();
+		for (String runWindows : allWindows) {
+			driver.switchTo().window(runWindows);
+			String currentWin = driver.getTitle();
+			if (currentWin.equals(title)) {
+				break;
+			}
+		}
+	}
+
+	public boolean closeAllWithoutParentWindows(String parentWindow) {
+		Set<String> allWindows = driver.getWindowHandles();
+		for (String runWindows : allWindows) {
+			if (!runWindows.equals(parentWindow)) {
+				driver.switchTo().window(runWindows);
+				driver.close();
+			}
+		}
+		driver.switchTo().window(parentWindow);
+		if (driver.getWindowHandles().size() == 1)
+			return true;
+		else
+			return false;
 	}
 
 	@BeforeClass
 	public void beforeClass() {
 		driver = new FirefoxDriver();
-		driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 	}
 
