9e0e464fc821e28cd02b497ea1e9bda3cdd16d56
Ha Nguyen <thanhhanguyenc10d@gmail.com>
2018-09-12 17:34:22 +0700

    Topic 10 - TC 03

--- src/selenium_api/Topic_10_Wait.java
+++ src/selenium_api/Topic_10_Wait.java
@@ -1,8 +1,12 @@
 package selenium_api;
 
 import org.testng.annotations.Test;
+
+import com.google.common.base.Function;
+
 import org.testng.annotations.BeforeClass;
 
+import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -10,13 +14,14 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 
 public class Topic_10_Wait {
 
-	WebDriver driver;	
+	WebDriver driver;
 	WebDriverWait wait;
 
 	public void TC_01_implicitWait() {
@@ -28,33 +33,62 @@ public class Topic_10_Wait {
 		Assert.assertTrue(driver.findElement(By.xpath("//h4[text()='Hello World!']")).isDisplayed());
 	}
 
-	@Test
+	// @Test
 	public void TC_02_explicitWait() {
 
 		driver.get("https://demos.telerik.com/aspnet-ajax/ajaxloadingpanel/functionality/explicit-show-hide/defaultcs.aspx");
 
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='calendarContainer']")));
-		
+
 		WebElement selectedDate = driver.findElement(By.xpath("//span[@id='ctl00_ContentPlaceholder1_Label1']"));
-		
+
 		Assert.assertEquals(selectedDate.getText(), "No Selected Dates to display.");
-		
+
 		driver.findElement(By.xpath("//tr[@class='rcRow']//a[text()='11']")).click();
-		
+
 		wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//div[@class='raDiv']")));
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//td[@class='rcSelected']//a[text()='11']")));
-		
+
 		Assert.assertEquals(driver.findElement(By.xpath("//span[@id='ctl00_ContentPlaceholder1_Label1']")).getText(), "Tuesday, September 11, 2018");
-		
+
 	}
 
+	@Test
+	public void TC_03_fluentWait() {
+		
+		driver.get("https://daominhdam.github.io/fluent-wait/");
+		
+		WebElement countdount =  driver.findElement(By.xpath("//div[@id='javascript_countdown_time']"));
+		wait.until(ExpectedConditions.visibilityOf(countdount));
+
+		// Khởi tạo Fluent wait
+		new FluentWait<WebElement>(countdount)
+		           // Tổng time wait là 15s
+		           .withTimeout(15, TimeUnit.SECONDS)
+		            // Tần số mỗi 1s check 1 lần
+		            .pollingEvery(500, TimeUnit.MILLISECONDS)
+		           // Nếu gặp exception là find ko thấy element sẽ bỏ  qua
+		            .ignoring(NoSuchElementException.class)
+		            // Kiểm tra điều kiện
+		            .until(new Function<WebElement, Boolean>() {
+		                public Boolean apply(WebElement element) {
+		                           // Kiểm tra điều kiện countdount = 00
+		                           boolean flag =  element.getText().endsWith("00");
+		                           System.out.println("Time = " +  element.getText());
+		                           // return giá trị cho function apply
+		                           return flag;
+		                      }
+		               });
+		
+	}
+	
 	@BeforeClass
 	public void beforeClass() {
 
 		driver = new FirefoxDriver();
 		driver.manage().timeouts().implicitlyWait(05, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
-		
+
 		wait = new WebDriverWait(driver, 30);
 	}
 
