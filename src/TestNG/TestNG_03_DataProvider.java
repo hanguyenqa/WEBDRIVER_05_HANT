a8cc0b8a2b287be535b5390b0d35130258d3535d
Ha Nguyen <Ha Nguyen@192.168.1.6>
2018-09-17 21:56:48 +0700

    Exercise topic 12 TestNG - Done

--- src/TestNG/TestNG_03_DataProvider.java
+++ src/TestNG/TestNG_03_DataProvider.java
@@ -3,16 +3,20 @@ package TestNG;
 import org.testng.annotations.Test;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 
 public class TestNG_03_DataProvider {
-	
+
 	WebDriver driver;
 
-	@BeforeClass
-	public void beforeClass() {
+	@BeforeMethod
+	public void beforeMethod() {
 		driver = new FirefoxDriver();
 		driver.get("http://live.guru99.com/index.php/customer/account/login/");
 	}
@@ -20,6 +24,12 @@ public class TestNG_03_DataProvider {
 	@Test(dataProvider = "accounts")
 	public void TC_LoginToSystem(String username, String password) {
 
+		driver.findElement(By.xpath("//input[@id='email']")).sendKeys(username);
+		driver.findElement(By.xpath("//input[@id='pass']")).sendKeys(password);
+		driver.findElement(By.xpath("//button[@id='send2']")).click();
+		
+		Assert.assertEquals(driver.findElement(By.xpath("//h1[text()='My Dashboard']")).isDisplayed(), true);
+		
 	}
 
 	@DataProvider
@@ -27,8 +37,10 @@ public class TestNG_03_DataProvider {
 		return new Object[][] { new Object[] { "automationvalid_01@gmail.com", "111111" }, new Object[] { "automationvalid_02@gmail.com", "111111" }, new Object[] { "automationvalid_03@gmail.com", "111111" } };
 	}
 
-	@AfterClass
-	public void afterClass() {
+	@AfterMethod
+	public void afterMethod() {
+		
+		driver.quit();
 	}
 
 }
